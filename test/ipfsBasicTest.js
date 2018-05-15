f870551c360f2a5a4e9f723339af93b8161ea98c
Mark Shwartzman <markshw@gmail.com>
2018-05-14 23:44:15 -0700

    1st test

--- test/ipfsBasicTest.js
+++ test/ipfsBasicTest.js
@@ -3,8 +3,70 @@ const should = require('chai')
     .should();
 
 describe('basic IPFS test', async () => {
-    it('add file to IPFS', async () => {
+    it('check mocha setup', async () => {
         let one=1;
         one.should.be.equal(1);
     });
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+
+    it('add file to IPFS',  async () => {
+        const IPFS = require('ipfs');
+        const ipfs =  new IPFS();
+
+        let ready = false;
+        while (!ready)
+        {
+            ipfs.on('ready', () => {
+                ready=true;
+            });
+            console.log('IPFS node starting up...');
+            await sleep(100);
+        }
+       
+        console.log('IPFS node is ready');
+        
+        const version = await ipfs.version()
+        console.log('ipfs version: ', version);
+        
+        await ipfs.stop()
+        console.log('Node stopped!')
+        
+        
+    });
+
+    // it('add file to IPFS', async () => {
+    //     const IPFS = require('ipfs');
+    //     const ipfs =  new IPFS();
+
+    //     let ready = false;
+    //     while (!ready)
+    //     {
+    //         ipfs.on('ready', () => {
+    //             ready=true;
+    //         });
+    //         console.log('IPFS node starting up...');
+    //         await sleep(100);
+    //     }
+       
+    //     console.log('IPFS node is ready');
+            
+    //     ipfs.version()
+    //     .then((version) => {
+    //         console.log('ipfs version: ', version);
+            
+    //         ipfs.stop()
+    //         .then(() => console.log('Node stopped!'))
+    //         .catch(error => console.error('Node failed to stop cleanly!', error))
+    //     })
+    //     .catch((error)=>console.error('failed to get version', error))   
+    // });
+
+    it('last test',  () => {
+        console.log('finish');
+    });
+
+    
 });
